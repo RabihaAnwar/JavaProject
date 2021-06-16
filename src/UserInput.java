28354c1aa1c212f9a68f115e1ec6223e3e435f08
Rabiha <rabiha.a.hussain@gmail.com>
2021-06-16 20:04:25 +0500

    added doWhile & commments

--- src/UserInput.java
+++ src/UserInput.java
@@ -4,10 +4,20 @@ public class UserInput {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		  System.out.println("Enter a decimal: "); 
-		  double dval = input.nextDouble();
-		  System.out.println("Your decimal value is: " + dval);
-		 
+		/*
+		 * System.out.println("Enter a decimal: "); double dval = input.nextDouble();
+		 * System.out.println("Your decimal value is: " + dval);
+		 */
+		
+		int val = 0;
+		do {
+			System.out.println("Enter a number: ");
+			val = input.nextInt();
+		}
+		
+		while (val != 9); {
+			System.out.println("You can't enter 9!");
+		}
 		
 			
 	}
