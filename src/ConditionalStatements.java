375767f2d26dc98a443d140bee869c2f4e43cd7a
Rabiha <rabiha.a.hussain@gmail.com>
2021-06-16 19:38:54 +0500

    added Conditional Statments demo

---
+++ src/ConditionalStatements.java
@@ -0,0 +1,15 @@
+
+public class ConditionalStatements {
+public static void main(String[] args) {
+	
+	int amount = 30;
+	
+	if (amount == 25 ) {
+		System.out.println("True!");
+	}
+	
+	else {
+		System.out.println("False!");
+	}
+}
+}
