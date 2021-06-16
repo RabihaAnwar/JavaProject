07b972270928b68512ee347eafe689a158d11e74
Rabiha <rabiha.a.hussain@gmail.com>
2021-06-16 18:37:49 +0500

    First commit
    Added all Project1 files

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+public static void main(String[] args) {
+	int val = 5;
+	double dval = 5.456;
+	/*
+	 * String texta = "Hello"; String textb = "World";
+	 * System.out.println(texta+" "+textb);
+	 */
+	
+	System.out.println("Result: " + dval+",,,,,,"+val);
+}
+}
