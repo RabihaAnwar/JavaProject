9ccacb69f18eff52b81ff17587cbf21f70cf8ad0
Rabiha <rabiha.a.hussain@gmail.com>
2021-06-16 18:47:58 +0500

    removed extra commas

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	 * System.out.println(texta+" "+textb);
 	 */
 	
-	System.out.println("Result: " + dval+",,,,,,"+val);
+	System.out.println("Result: " + dval+","+val);
 	System.out.println("Result: " + val);
 }
 }
