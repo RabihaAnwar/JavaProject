ff64a66f36db5449b4a265c68654c9fa914da42f
Rabiha <rabiha.a.hussain@gmail.com>
2021-06-16 20:02:17 +0500

    added UserInput file

---
+++ src/UserInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		  System.out.println("Enter a decimal: "); 
+		  double dval = input.nextDouble();
+		  System.out.println("Your decimal value is: " + dval);
+		 
+		
+			
+	}
+}
